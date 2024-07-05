13b9e6cdad370e459aecd4885681119e9a7340e4
Elena Vereskun <lena.vereskun@yandex.ru>
2024-07-05 17:33:39 +0300

    fix import

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
-import Header from "./src/components/Header/Header";
 import { NewItemsTitle, Slider } from "./src/components/NewItems/NewItems";
 import SliderMain from "./src/components/SliderMain/SliderMain";
 import Collections from "./src/components/Collections/Collections";
 import MenuNavBar from "./src/components/Menu/MenuNavBar/MenuNavBar";
+import Header from "./src/components/Header/Header";
 
 export default function Home() {
   return (
